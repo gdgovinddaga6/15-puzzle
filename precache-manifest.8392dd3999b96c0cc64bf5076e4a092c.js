399a4a893310d906317a5bab1f9287e177062f45
Dattatreya Garimella <dattatreya.garimella@ggktech.com>
2019-09-17 16:17:10 +0530

    Updates

---
+++ precache-manifest.8392dd3999b96c0cc64bf5076e4a092c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78861700de7bc40f58f7f67f98e4ff93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf59f29edea0c78923da",
+    "url": "/static/js/2.00b210ae.chunk.js"
+  },
+  {
+    "revision": "9ff885056c10d227014e",
+    "url": "/static/js/main.421987f3.chunk.js"
+  },
+  {
+    "revision": "496a8d91fd073135e3ed",
+    "url": "/static/js/runtime~main.4277c920.js"
+  }
+]);
